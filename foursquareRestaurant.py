ef0116687f1d88515535322b1d5e26a6f5a078c0
Tyler <tbwolfe@live.com>
2017-11-25 14:47:46 -0500

    Update foursquareRestaurant.py

--- foursquareRestaurant.py
+++ foursquareRestaurant.py
@@ -1,4 +1,3 @@
-from geo import getGeocodeLocation
 import json
 import httplib2
 import sys
@@ -7,9 +6,19 @@ import codecs
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
-foursquare_client_id = "HIDDEN_USE_YOUR_ID_HERE"
-foursquare_client_secret = "HIDDEN_USE_YOUR_SECRET_KEY_HERE"
+foursquare_client_id = "YOUR_ID_HERE"
+foursquare_client_secret = "YOUR_SECRET_HERE"
+google_api_key = "YOUR_KEY_HERE"
 
+
+def getGeocodeLocation(inputString):
+    locationString = inputString.replace(" ", "+")
+    url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s' % (locationString, google_api_key))
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    latitude = result['results'][0]['geometry']['location']['lat']
+    longitude = result['results'][0]['geometry']['location']['lng']
+    return (latitude, longitude)
 
 def findARestaurant(mealType, location):
     latitude, longitude = getGeocodeLocation(location)
@@ -17,7 +26,6 @@ def findARestaurant(mealType, location):
     foursquare_client_id, foursquare_client_secret, latitude, longitude, mealType))
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
-
     if result['response']['venues']:
         restaurant = result['response']['venues'][0]
         venue_id = restaurant['id']
@@ -27,6 +35,7 @@ def findARestaurant(mealType, location):
         for i in restaurant_address:
             address += i + " "
         restaurant_address = address
+
         url = ('https://api.foursquare.com/v2/venues/%s/photos?client_id=%s&v=20150603&client_secret=%s' % (
         (venue_id, foursquare_client_id, foursquare_client_secret)))
         result = json.loads(h.request(url, 'GET')[1])
@@ -37,20 +46,20 @@ def findARestaurant(mealType, location):
             imageURL = prefix + "300x300" + suffix
         else:
             imageURL = "http://pixabay.com/get/8926af5eb597ca51ca4c/1433440765/cheeseburger-34314_1280.png?direct"
+
         restaurantInfo = {'name': restaurant_name, 'address': restaurant_address, 'image': imageURL}
-        print "Restaurant Name: %s" % restaurantInfo['name']
-        print "Restaurant Address: %s" % restaurantInfo['address']
-        print "Image: %s \n" % restaurantInfo['image']
         return restaurantInfo
     else:
-        print "No Restaurants Found for %s" % location
         return "No Restaurants Found"
 
 
 if __name__ == '__main__':
-    findARestaurant("Sushi", "Tokyo, Japan")
-    findARestaurant("Ice Cream", "Cairo, Egypt")
+    findARestaurant("Pizza", "Tokyo, Japan")
+    findARestaurant("Tacos", "Jakarta, Indonesia")
+    findARestaurant("Tapas", "Maputo, Mozambique")
+    findARestaurant("Falafel", "Cairo, Egypt")
+    findARestaurant("Spaghetti", "New Delhi, India")
     findARestaurant("Cappuccino", "Geneva, Switzerland")
-    findARestaurant("Tacos", "Los Angeles, California")
+    findARestaurant("Sushi", "Los Angeles, California")
     findARestaurant("Steak", "La Paz, Bolivia")
-    findARestaurant("Gyros", "Sydney Australia")
+    findARestaurant("Gyros", "Sydney Austrailia")
